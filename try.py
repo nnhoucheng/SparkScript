81ba1174b1be809dcf8e0fe20e27b7111c8f3dbb
侯程 <nnhoucheng@nnhc-Air.fios-router.home>
2017-04-26 15:21:40 -0400

    change try.py

--- try.py
+++ try.py
@@ -1,9 +1,7 @@
-import sys
+from pyspark import SparkContext
 
-def printfirstline():
-    for line in sys.stdin:
-        print line
-	break
-
-if __name__=='__main__'
-    printfirstline()
+if __name__=='__main__':
+    sc = SparkContext()
+    f = sc.textFile('/tmp/citibike.csv', use_unicode=False).cache()
+    for i,c in enumerate(f.first().split(',')):
+        print("{} : {}".format(i,c)) 
