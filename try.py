8ef307224799a89dfd78ed84fa87b72bde868a34
侯程 <nnhoucheng@nnhc-Air.fios-router.home>
2017-04-26 15:43:42 -0400

    update try.py write to file

--- try.py
+++ try.py
@@ -3,5 +3,6 @@ from pyspark import SparkContext
 if __name__=='__main__':
     sc = SparkContext()
     f = sc.textFile('/tmp/citibike.csv', use_unicode=False).cache()
-    for i,c in enumerate(f.first().split(',')):
-        print("{} : {}".format(i,c)) 
+    with open('tmp.txt', 'wb') as fo:
+        for i,c in enumerate(f.first().split(',')):
+            fo.write("{} : {}\n".format(i,c)) 
