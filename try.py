1b6556f174f37ddfb214b92b8eb95dd446977974
侯程 <nnhoucheng@172-16-23-1.DYNAPOOL.NYU.EDU>
2017-04-26 19:16:37 -0400

    try to use argv

--- try.py
+++ try.py
@@ -1,8 +1,11 @@
 from pyspark import SparkContext
+import sys
 
 if __name__=='__main__':
     sc = SparkContext()
-    f = sc.textFile('/tmp/citibike.csv', use_unicode=False).cache()
-    with open('tmp.txt', 'wb') as fo:
-        print '\n'*10
-        print "{}".format(f.count())
+    
+    if len(sys.argv) == 2:
+        f = sc.textFile(sys.argv[1], use_unicode=False).cache()
+        with open('tmp.txt', 'wb') as fo:
+            print '\n'*10
+            print "{}".format(f.count())
