258c543d0129b2e98f0a1005b262f22c92ea2243
侯程 <nnhoucheng@nnhc-Air.fios-router.home>
2017-04-26 15:47:29 -0400

    update try.py 2

--- try.py
+++ try.py
@@ -4,5 +4,6 @@ if __name__=='__main__':
     sc = SparkContext()
     f = sc.textFile('/tmp/citibike.csv', use_unicode=False).cache()
     with open('tmp.txt', 'wb') as fo:
+        fo.write("first line\n")
         for i,c in enumerate(f.first().split(',')):
             fo.write("{} : {}\n".format(i,c)) 
