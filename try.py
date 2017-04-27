baf841d9410067edf822016bcd28c57847e65791
侯程 <nnhoucheng@172-16-23-210.DYNAPOOL.NYU.EDU>
2017-04-27 12:48:30 -0400

    try:print column 2

--- try.py
+++ try.py
@@ -6,6 +6,8 @@ if __name__=='__main__':
     
     if len(sys.argv) == 2:
         f = sc.textFile(sys.argv[1], use_unicode=False).cache()
+        cs = f.first().split(',')
         print '\n'*10
-        for i,c in enumerate(f.first()):
+        for i,c in enumerate(cs):
             print "{} : {}".format(i,c)
+        print '\n'*10
