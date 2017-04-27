2d22b368e5a2ea58e78cb51bc3d9384d656c3656
侯程 <nnhoucheng@172-16-23-210.DYNAPOOL.NYU.EDU>
2017-04-27 12:42:54 -0400

    try:print column

--- try.py
+++ try.py
@@ -1,10 +1,11 @@
-#from pyspark import SparkContext
+from pyspark import SparkContext
 import sys
 
 if __name__=='__main__':
-    #sc = SparkContext()
+    sc = SparkContext()
     
     if len(sys.argv) == 2:
         f = sc.textFile(sys.argv[1], use_unicode=False).cache()
         print '\n'*10
-        print "{}".format(f.count())
+        for i,c in enumerate(f.first()):
+            print "{} : {}".format(i,c)
